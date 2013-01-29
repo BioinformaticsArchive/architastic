d6bae2f64593d24947bd0770740fcea843ab5fa4
Mark T. Holder <mtholder@gmail.com>
2013-01-29 14:31:57 -0600

    getting a URL from TNRS

--- controllers/tree.py
+++ controllers/tree.py
@@ -1,3 +1,37 @@
+import requests
+
+def _get_tnrs_uri(submit_uri, name_list):
+    names_newline_sep = '\n'.join(name_list)
+    resp = requests.get(submit_uri,
+                        params={'query':names_newline_sep},
+                        allow_redirects=True)
+    resp.raise_for_status()
+    results = resp.json()
+    retrieve_uri = results.get(u'uri')
+    if retrieve_uri:
+        return retrieve_uri
+    elif len(resp.history) > 0:
+        return resp.url
+    else:
+        raise HTTP(503) # not sure if there is a better status code to return here...
+b='''        sys.exit('Did not get a URI or redirect from the submit GET operation. Got:\n %s\n' % str(results))
+        min_time = time.time()
+        sleep_interval = 1.0
+        while retrieve_uri:
+            retrieve_response = requests.get(retrieve_uri)
+            retrieve_response.raise_for_status()
+            retrieve_results = retrieve_response.json()
+            if NAMES_KEY in retrieve_results:
+                break
+            min_time = time.time()
+            sys.stderr.write('Waiting (%f sec) for processing by tnrs.\n' % sleep_interval)
+            time.sleep(sleep_interval)
+            sleep_interval *= sleep_interval_increase_factor
+            
+        write_resolved_names(this_batch, retrieve_results[NAMES_KEY], sys.stdout)
+        curr_ind += batch_size
+'''
+
 def enter():
     form = FORM(INPUT(_name='taxa', 
                       _type='TEXTAREA',
@@ -9,8 +43,14 @@ def enter():
 def find():
     raw_taxa_str = request.vars.taxa
     taxa_list = [i.strip() for i in raw_taxa_str.split(',')]
+    
+    #@TEMP should be based on the user's TNRS choice...
+    domain = 'http://api.phylotastic.org'
+    submit_path = 'tnrs/submit'
+    submit_uri = domain + '/' + submit_path
 
-    new_id = db.tax_query.insert(url='http://api.phylotastic.org/tnrs/retrieve/14b530dae634f330147d0a54089d87ed')
+    u = _get_tnrs_uri(submit_uri, taxa_list)
+    new_id = db.tax_query.insert(url=u)
     return redirect(URL('show', args=(new_id,)))
 
 def show():
