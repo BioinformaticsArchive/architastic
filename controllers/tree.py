0f60c003b0d57035ead99177dc59fc58faaf1d8f
Karen Cranston <karen.cranston@gmail.com>
2013-01-29 12:57:24 -0700

    Modified form

--- controllers/tree.py
+++ controllers/tree.py
@@ -1,9 +1,13 @@
 def test():
-    form = FORM(INPUT(_name='taxa', 
+    form = FORM("List of taxa: ",INPUT(_name='taxa', 
     	              _type='TEXTAREA',
     	              requires=IS_NOT_EMPTY()),
-                INPUT(_type='submit'),
-                _action=URL('second'))
+                INPUT(_type='submit'))
+    if form.accepts(request,session):
+        response.flash='form accepted'
+    elif form.errors:
+        response.flash='form has errors'
+    else:
+        response.flash='please complete form'
     return dict(form=form)
-def second():
-    return request.vars.taxa
+
