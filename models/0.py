ec4cd49a5d898b234facaad4d6b7c5e1cad2be0e
Mark T. Holder <mtholder@gmail.com>
2013-01-29 09:56:23 -0600

    initial commit

---
+++ models/0.py
@@ -0,0 +1,19 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'Phylotastic TreeStore'
+settings.subtitle = 'powered by web2py'
+settings.author = 'Karen Cranston'
+settings.author_email = 'karen.cranston@nescent.org'
+settings.keywords = ''
+settings.description = ''
+settings.layout_theme = 'Default'
+settings.database_uri = 'sqlite://storage.sqlite'
+settings.security_key = '9cc29ccc-07cb-4813-9b4d-f65b6f52ac64'
+settings.email_server = 'localhost'
+settings.email_sender = 'you@example.com'
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
+settings.plugins = []
