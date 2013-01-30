84f4b1f75a3d9d0f3a6ce19fb6f23f682537e0e7
Mark T. Holder <mtholder@gmail.com>
2013-01-30 12:52:07 -0600

    proxy_tnrs not crashing...

--- controllers/tree.py
+++ controllers/tree.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def _get_tnrs_uri(submit_uri, name_list):
     names_newline_sep = '\n'.join(name_list)
@@ -69,8 +70,9 @@ def find():
         db.name_from_user.insert(tax_query=new_id,
                                  original_name=name,
                                  tnrs_json='',
+                                 taxon_name='',
                                  taxon_uri='',
-                                 match_method='')
+                                 match_status='')
     return redirect(URL('show', args=(new_id,)))
 
 # Shows results from TNRS call
@@ -85,11 +87,119 @@ def show():
             'name_row_list' : name_row_list, 
             'tax_query_id': q_id }
 
+class NameMatchingTypeFacets:
+    PERFECT_AMBIGUOUS = 'multiple perfect matches'
+    IMPERFECT_AMBIGUOUS = 'imperfect match and there are multiple matches'
+    UNRECOGNIZED = 'not recognized by TNRS'
+    NOT_IN_STORE = 'recognized by TNRS but not in tree store'
+    USER = 'user matched'
+    ONLY_PERFECT_IN_STORE = 'only perfect match in tree store'
+    ONLY_MATCH_IN_STORE = 'imperfect but only match in tree store'
+    UNCHECKED = ''
+
+#@ TEMP need to get the names from the tree_store to answer this correctly...
+def _is_known_name(name_uri_tuple, source, tree_store_matching_context):
+    name, uri = name_uri_tuple
+    return True
+
+# grab JSON from the TNRS and return it. This avoids problems with cross-domain scripting
+#   restriction
 def proxy_tnrs():
     try:
         q_id = request.args[-1]
         q = db.tax_query[q_id]
     except:
         raise HTTP(404)
+    name_row_list = db(db.name_from_user.tax_query == q).select()
+    
     response.headers['content-type'] = 'json'
-    return requests.get(q.url).text
+    json2return = {}
+    name_row_list = db(db.name_from_user.tax_query == q).select()
+    for row in name_row_list:
+        if row.match_status == NameMatchingTypeFacets.UNCHECKED:
+            all_matches = []
+            mj = json.dumps(all_matches)
+            match_status = NameMatchingTypeFacets.UNRECOGNIZED
+            row.update(tnrs_json=mj,
+                       match_status=match_status,
+                       taxon_name='',
+                       taxon_uri='')
+            row_key = str(q_id) + ' ' + str(row.id)
+            json2return[row_key] = {
+                'tnrs_query_id' : q_id,
+                'name_id_in_query' : str(row.id),
+                'allMatches' : all_matches,
+                'matchStatus' : match_status,
+                'taxonName' : '',
+                'taxonUri' : '',
+                'submittedName' : str(row.original_name)
+            }
+    if len(json2return) > 0:
+        json.dumps(json2return)
+    resp = requests.get(q.url)
+    data = resp.json()
+    matchedList = data['names']
+    tree_store_matching_context = None #@TEMP dummy placeholder
+    for matchBlob in matchedList:
+        all_matches = matchBlob['matches']
+        perfect_matches = []
+        known_matches = []
+        for curr_match in all_matches:
+            source = curr_match['sourceId']
+            m_n = curr_match['matchedName']
+            m_u = curr_match['uri']
+            is_in_tree_store = _is_known_name((m_n, m_u), source, tree_store_matching_context)
+            is_perfect_match = curr_match['score'].startswith('1')
+            curr_match['is_in_tree_store'] = is_in_tree_store
+            curr_match['is_perfect_match'] = is_perfect_match
+            curr_match['is_only_known_perfect_match'] = False
+            if is_in_tree_store:
+                known_matches.append(curr_match)
+                if is_perfect_match:
+                    perfect_matches.append(curr_match)
+                    curr_match['match_status'] = NameMatchingTypeFacets.PERFECT_AMBIGUOUS
+                else:
+                    curr_match['match_status'] = NameMatchingTypeFacets.IMPERFECT_AMBIGUOUS
+            else:
+                curr_match['match_status'] = NameMatchingTypeFacets.NOT_IN_STORE
+        taxon_name = ''
+        taxon_uri = ''
+        if len(perfect_matches) == 1:
+            pm = perfect_matches[0]
+            pm['match_status'] = NameMatchingTypeFacets.ONLY_PERFECT_IN_STORE
+            match_status = NameMatchingTypeFacets.ONLY_PERFECT_IN_STORE
+            taxon_name = pm['acceptedName']
+            taxon_uri = pm['uri']
+        elif len(perfect_matches) > 1:
+            match_status = NameMatchingTypeFacets.PERFECT_AMBIGUOUS
+        elif len(known_matches) == 1:
+            km = known_matches[0]
+            km['match_status'] = NameMatchingTypeFacets.ONLY_MATCH_IN_STORE
+            match_status = NameMatchingTypeFacets.ONLY_MATCH_IN_STORE
+            taxon_name = km['acceptedName']
+            taxon_uri = km['uri']
+        elif len(known_matches) > 1:
+            match_status = NameMatchingTypeFacets.IMPERFECT_AMBIGUOUS
+        else:
+            match_status = NameMatchingTypeFacets.NOT_IN_STORE
+        mj = json.dumps(all_matches)
+        submitted_name = matchBlob['submittedName']
+        matched_db_row_list = db( (db.name_from_user.tax_query == q) & (db.name_from_user.original_name == submitted_name)).select()
+        for matched_db_row in matched_db_row_list:
+            matched_db_row.update(tnrs_json=mj,
+                                  match_status=match_status,
+                                  taxon_name=taxon_name,
+                                  taxon_uri=taxon_uri)
+            row_key = str(q_id) + ' ' + str(matched_db_row.id)
+            json2return[row_key] = {
+                'tnrs_query_id' : q_id,
+                'name_id_in_query' : str(matched_db_row.id),
+                'allMatches' : all_matches,
+                'matchStatus' : match_status,
+                'taxonName' : taxon_name,
+                'taxonUri' : taxon_uri,
+                'submittedName' : str(row.original_name)
+            }
+    db.commit()
+            
+    return json.dumps(json2return)
