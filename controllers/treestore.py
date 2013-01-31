0208dc9322a0240952276dc4f6d7f860c44307f5
Karen Cranston <karen.cranston@gmail.com>
2013-01-31 15:54:39 -0700

    importing taxa finally working

--- controllers/treestore.py
+++ controllers/treestore.py
@@ -3,6 +3,7 @@
  
 import json
 import requests
+import os
 
 # request names and identifiers from treestore
 
@@ -23,7 +24,8 @@ def _get_taxa_from_treestore(url):
 def getnames():
     form = SQLFORM.factory(
         Field('treestore',requires=IS_IN_SET(['opentree','rdf'])),
-        Field('json_dump_url',requires=IS_NOT_EMPTY()))
+        Field('json_dump_url',requires=IS_NOT_EMPTY()),
+        )
     if form.process().accepted:
         response.flash='input accepted'
         session.json_dump_url=form.vars.json_dump_url
@@ -33,28 +35,45 @@ def getnames():
         response.flash='form has errors'
     return dict(form=form)
 
+# perform some cursory checks on the results
+# from the treestore
+def _checknames():
+    phylodump = _get_taxa_from_treestore(session.json_dump_url)
+    datablock=phylodump['metadata']
+    ok=0
+    blocks=['names','version','treestoreMetadata','externalSources']
+    for text in blocks:
+        if datablock[text]:
+            ok=ok+1
+    if ok==4:
+        ok=1;
+    return int(ok)
+
 # inserts names into database
 # checks that identifier does not already exist
 # TODO: possible that name might change when identifer doesn't?
 def _insert_into_database(phylodump):
     inserted_names={};
-    if phylodump['names']:
-        names=phylodump['names']
-        for i in names:
-            name=i['name']
-            identifier=i['treestoreId']
-            rows = db(db.treestore_names.treestore_id==identifier).select()
-            if not rows:
-                inserted_names[identifier]=name
-                db.treestore_names.insert(taxon_name=name,treestore_id=identifier)
-    else:
-       response.flash='no names block'
+    datablock = phylodump['metadata']
+    try:
+        if datablock['names']:
+            names=datablock['names']
+            for i in names:
+                name=i['name']
+                identifier=i['treestoreId']
+                rows = db(db.treestore_names.treestore_id==identifier).select()
+                if not rows:
+                    inserted_names[identifier]=name
+                    db.treestore_names.insert(taxon_name=name,treestore_id=identifier)
+    except:
+        raise HTTP(404)
     return dict(inserted_names)
 
 def viewnames():
 #try:
     phylodump = _get_taxa_from_treestore(session.json_dump_url)
-    inserted_names=_insert_into_database(phylodump)
+    if _checknames():
+        inserted_names=_insert_into_database(phylodump)
     #except:
     #    raise HTTP(404)
     return locals()
